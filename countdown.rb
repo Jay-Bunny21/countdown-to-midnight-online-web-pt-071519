16b1f1784d0692a3e42740ee2494cd6219a9c18f
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-22 03:28:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,17 @@
 def countdown(num)
-
- while num > 0
-   puts "#{num} SECOND(S)!"
-  num -= 1
- end
-  "HAPPY NEW YEAR!"
+  num = 10
+while num > 0
+  puts "#{num} SECOND(S)!"
+    num -= 1
+end
 end
 
-def countdown_with_sleep(num)
 
- while num > 0
-   puts "#{num} SECOND(S)!"
-  sleep(1)
-  num -= 1
- end
-  "HAPPY NEW YEAR!"
-end	
+
+def countdown_with_sleep(num)
+x = sleep 1
+while x < 5
+puts "HAPPY NEW YEAR!"
+x -= 1
+end
+end
